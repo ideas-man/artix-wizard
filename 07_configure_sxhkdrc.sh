bbee687152e4cf5207a78b755f5a895f1a1f9829
ideas-man <ideasman.git@protonmail.com>
2022-01-04 14:27:19 +0100

    feat(sxhkdrc): added reload and backlight inc/dec bindings

--- 07_configure_sxhkdrc.sh
+++ 07_configure_sxhkdrc.sh
@@ -13,4 +13,7 @@ print_subheader ">>> Creating symlinks... <<<"
 mkdir -p $HOME/.config/sxhkd
 ln -sf $WIZARD_DOT/sxhkd/sxhkdrc $HOME/.config/sxhkd/sxhkdrc
 
+mkdir -p $HOME/.config/dunst
+ln -sf $WIZARD_DOT/dunst/dunstrc $HOME/.config/dunst/dunstrc
+
 print_subheader ">>> Done! <<<"
