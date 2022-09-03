b013cc847d34ce0224852cc103ec565a37c58465
ideas-man <ideasman.git@protonmail.com>
2022-09-03 21:36:08 +0200

    feat(dotfiles/gnupg.sh): more XDG-style paths

--- dotfiles/scripts/gnupg.sh
+++ dotfiles/scripts/gnupg.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-chmod 700 $HOME/.local/share/gnupg/
-chmod 600 $HOME/.local/share/gnupg/gpg.conf
-chmod 600 $HOME/.local/share/gnupg/gpg-agent.conf 
+chmod 700 $XDG_DATA_HOME/gnupg/
+chmod 600 $XDG_DATA_HOME/gnupg/gpg.conf
+chmod 600 $XDG_DATA_HOME/gnupg/gpg-agent.conf 
